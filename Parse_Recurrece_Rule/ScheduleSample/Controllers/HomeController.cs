ae4f325fc39b3300ac92b77896367538fb9f9d06
ROOT\umamaheswaric <umamaheswaric@syncfusion.com>
2018-12-12 17:17:58 +0530

    Sample structure added.

---
+++ Parse_Recurrece_Rule/ScheduleSample/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+
+namespace ScheduleSample.Controllers
+{
+    public class HomeController : Controller
+    {   
+        public ActionResult Index()
+        {  
+            return View();
+        }
+        
+        public JsonResult getDates(string dates)
+        {
+            var recurrenceRule = JsonConvert.DeserializeObject<string>(dates);
+            var dateCollection = RecurrenceHelper.GetRecurrenceDateTimeCollection(recurrenceRule, DateTime.Now);            
+            return Json(dateCollection, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
--- ScheduleSample/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ScheduleSample.MvcApplication" Language="C#" %>
